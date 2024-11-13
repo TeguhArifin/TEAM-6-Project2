90bdb21a2226f0b4e066f566d2525da2f21e223e
Teguh Arifin <106657065+TeguhArifin@users.noreply.github.com>
2024-11-13 18:28:43 +0700

    Delete project-isc/project-isc directory

--- project-isc/project-isc/js/logout.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("logoutButton").addEventListener("click", function () {
-  // Menghapus data pengguna dari localStorage
-  localStorage.removeItem("loggedInUser");
-  alert("Anda telah berhasil logout.");
-  // Redirect ke halaman login setelah logout
-  window.location.href = "main.html";
-});
